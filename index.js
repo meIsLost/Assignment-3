d1ddabd7bdfa5e4be12bd35fd04d4157ca5fe1d1
Sabin Bhandari <sabi5403@edu.easj.dk>
2022-09-27 14:24:25 +0200

    Assignment_3

---
+++ index.js
@@ -0,0 +1,25 @@
+Vue.createApp({
+    data() {
+        return {
+            message: null,
+            number: null,
+            result: ""
+        
+        }
+    },
+    methods: {
+        looper(message, number) {
+            if (number>0){
+                for (let i = 0; i < number; i++)
+                {
+                   this.result= this.result + message ;
+                }
+            }
+            else
+            {
+                    this.result = "be positive my friend." + this.number;
+            }
+            
+        }
+    }
+}).mount("#app")
